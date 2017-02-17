14be688859c752a2836c4d6513d279bbbcd7781d
Ian George <OppenheimerAndTheMartians@users.noreply.github.com>
2017-02-17 10:46:23 -0600

    Revert "Add goals"

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -5,67 +5,39 @@ var app = express();
 
 
 function getAllUsers(req, res, next) {
-  db.any('select * from User').then(function(data) {
+  db.any('select * from users').then(function(data) {
   	res.status(200).json({
   		status: 'success', 
-  		data: data, 
-  		message: 'RETREIVED ALL USERS'
-  	});
-  }).catch(function (err) {
-  	return next(err);
-  });
-}
-
-function getSingleUser(req, res, next) {
-  var id = parseInt(req.params.id);
-  db.one('select * from Users where id = $1', id).then(function (data) {
-  	res.status(200).json({
-  		status: 'success',
-  		data: data,
-  		message: 'GOT ONE USER FROM DATABASE: ID' + id;
+  		data: data, message: 
+  		'RETREIVED ALL USERS'
   	})
   }).catch(function (err) {
   	return next(err);
-  });
+  })
 }
 
-function getSetting(req, res, next) {
-  var settings = req.params.setting_id;
-  db.one('select * from Users where setting_id = $1', req.params.settings).then(function (data) {
+function getSingleUser(req, res, next) {
+  var userName = req.params.username;
+  db.one('select * from users where id = $1', userName).then(function (data) {
   	res.status(200).json({
   		status: 'success',
   		data: data,
-  		message: 'GOT SETTINGS FROM DATABASE: ID' + id;
-  	});
+  		message: 'GOT ONE USER FROM DATA BASE: NAME' + userName;
+  	})
   }).catch(function (err) {
-    return next(err);
-  });
-}
-
-function getAllGoals(req, res, next) {
-  db.any('select * from Goal').then(function (data) {
-    res.status(200).json({
-      status: 'success',
-      data: data,
-      message: 'RETREIVED ALL GOALS'
-    });
-  }).catch(function (err) {
-    return next(err);
+  	return next(err);
   })
 }
 
-function postAllGoals(req, res, next) {
-  db.none('insert into Goal(goal_id, progress, subgoal_id, goal_picture + goal + reflection_id)' 
-    + 'values(${goal_id} + ${progress} + ${subgoal_id} + ${goal_picture} + ${goal} + ${reflection_id})', req.body)
-  .tnen(function () {
-    res.status(200).json({
-      status: 'success'
-      message: 'Inserted goals'
-    });
-  }).catch(function (err) {
-    return next(err);
-  })
+function getSetting(req, res, next) {
+  var settings = req.params.username.setting;
+  db.one('select * from users')
 }
+
+function getBlackList(req, res, next) {
+
+}
+
 
 module.exports = {
 	getAllUsers: getAllUsers,
