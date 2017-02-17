13a131fcb6201d15a09a45e33775bd6abc80e596
igeorge <irgeorge@gmx.com>
2017-02-17 09:38:41 -0600

    Added first controller functions

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,15 +1,46 @@
 var models = require('../server/databases/Models')
 var url = require('url');
+var db = require('');
+var parser  = require('body-parser');
 var express = require('express');
 var app = express();
-var parser  = require('body-parser');
 
 
 function getAllUsers(req, res, next) {
+  db.any('select * from users').then(function(data) {
+  	res.status(200).json({
+  		status: 'success', 
+  		data: data, message: 
+  		'RETREIVED ALL USERS'
+  	})
+  }).catch(function (err) {
+  	return next(err);
+  })
+}
+
+function getSingleUser(req, res, next) {
+  var userName = req.params.username;
+  db.one('select * from users where id = $1', userName).then(function (data) {
+  	res.status(200).json({
+  		status: 'success',
+  		data: data,
+  		message: 'GOT ONE USER FROM DATA BASE: NAME' + userName;
+  	})
+  }).catch(function (err) {
+  	return next(err);
+  })
+}
+
+function getSetting(req, res, next) {
+
+}
+
+function getBlackList(req, res, next) {
 
 }
 
 
 module.exports = {
-	getAllUsers: getAllUsers
+	getAllUsers: getAllUsers,
+	getSingleUser: getSingleUser
 }
