b91a1f0034e809aeea4412b822c1a59e17790086
Josephine-Chen <jychen@g.hmc.edu>
2017-02-16 17:11:24 -0800

    Set up bare bones

---
+++ server/server.js
@@ -0,0 +1,13 @@
+//Import routes from another file
+var express = require('express');
+var bodyparser = require('body-parser');
+var path = require('path');
+var app = express();
+
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, '../client')));
+
+var port = process.env.PORT || 7777;
+app.listen(port, function() {
+  console.log('Listening on ', port);
+});
