68a2eb58d939856a6b4159c7929d096f8061f50b
Josephine-Chen <jychen@g.hmc.edu>
2017-02-17 17:22:55 -0800

    Set up bare bones landing page and dashboard

---
+++ client/src/components/Landingpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {} from 'react-materialize';
+
+export default class Landingpage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1> FocusPro </h1>
+      <h5> Welcome to PhocusPro. <br /> We help you limit distractions and stay on task. </h5>
+      </div>
+    );
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import { render } from 'react-dom';
 import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
 
 import App from './components/App.js';
+import Landingpage from './components/Landingpage.js';
+import Dashboard from './components/Dashboard.js';
 
 const app = document.getElementById('app');
 
 render(
   <Router history={hashHistory}>
     <Route path="/" component = {App}>
+    <IndexRoute component={Landingpage}></IndexRoute>
+    <Route path="dashboard" component={Dashboard}/>
     </Route>
   </Router>
   , app
