6aa9fbff42c9ab8d1f661896e8fe5c8078526239
Josephine-Chen <jychen@g.hmc.edu>
2017-02-18 15:37:26 -0800

    Change UI for full view and mobile view

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
+import {Grid, Row, Col} from 'react-materialize'
 export const Sidebar =()=>(
 
   <header>
-      <nav className="top-nav">
-        <div className="container">
-          <div className="nav-wrapper"><a className="page-title">FocusPro</a></div>
-        </div>
+      <nav className="top-nav full hide-on-large-only">
+          <div className="nav-wrapper"><a href="#" data-activates="slide-out" className="button-collapse top-nav full hide-on-large-only"><i className="material-icons">menu</i></a></div>
       </nav>
-      <div className="container"><a href="#" data-activates="slide-out" className="button-collapse top-nav full hide-on-large-only"><i className="material-icons">menu</i></a></div>
-
       <ul id="slide-out" className="side-nav fixed theme-color">
         <li><div className="userView">
           <a href="#"><img className="circle" /></a>
--- client/src/custom.js
+++ client/src/custom.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
   //$('.collapsible').collapsible();
   // Initialize collapse button
     $('.button-collapse').sideNav({
-      // menuWidth: 200, // Default is 300
+      menuWidth: 200, // Default is 300
       // edge: 'left', // Choose the horizontal origin
       // closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       // draggable: true // Choose whether you can drag to open on touch screens
