9390c2bf31e8e15f443d0a583fee25ca3271a0bc
SherryH <enjoy.wind7rain@gmail.com>
2017-02-18 12:44:21 -0800

    Add Enzyme test and Jest Snapshot test

---
+++ client/src/__test__/sidebar.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Sidebar} from '../components/Sidebar.js';
+import { shallow, mount } from 'enzyme';
+
+describe('Sidebar', () => {
+  let sidebar;
+
+  beforeAll(() => {
+    sidebar = shallow(<Sidebar />);
+  });
+
+  it('Sidebar renders title', () => {
+    expect(sidebar.find('.page-title').text()).toEqual('FocusPro');
+  });
+});
+
+describe('Sidebar (Snapshot)', () => {
+  it('Sidebar renders', () => {
+    const component = renderer.create(<Sidebar />);
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+});
