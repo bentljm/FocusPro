d13a55af0b42bbb0e872bcbf359b3032c1bc96b1
Jeff Bentler <bentljm@gmail.com>
2017-02-18 10:42:50 -0800

    Create components and add to routing

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {} from 'react-materialize';
+import Goal from './Goal.js';
 
 export default class Dashboard extends React.Component {
   render() {
     return (
       <div>
-      <h1> Welcome, USER </h1>
-
+        <h1> Welcome, USER </h1>
+        <Goal />
       </div>
     );
   }
---
+++ client/src/components/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from 'react-materialize';
+
+export default class Settings extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Settings </h1>
+      </div>
+    );
+  }
+}
