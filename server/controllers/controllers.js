d6f4e25ef30781f21cd6f2b678600e47d1d554e8
Ian George <OppenheimerAndTheMartians@users.noreply.github.com>
2017-02-18 19:19:18 -0600

    Update controllers.js

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,170 +1,212 @@
-var db = require('../databases/Schema.js');
+var db = require('../server/databases/Schema');
 var parser  = require('body-parser');
 var express = require('express');
+var app = express();
 
 
-// GETTERS
+function getAllUsers(req, res, next) {
+  db.any('select * from User').then(function(data) {
+  	res.status(200).json({
+  		status: 'success', 
+  		data: data, 
+  		message: 'RETREIVED ALL USERS'
+  	});
+  }).catch(function (err) {
+  	return next(err);
+  });
+}
 
-function getAllUsers(req, res, next) { // Get all users from database
-  db.any('select * from User').then(function(data) { // Get all data from user database
-    res.status(200).json({ // and send a GET.
-      status: 'success',
-      data: data,
-      message: 'RETREIVED ALL USERS'
-    });
-  }).catch(function (err) { // ERROR HANDLING
+function getSingleUser(req, res, next) {
+  var username = parseInt(req.params.username);
+  db.one('select * from Users where username = $1', username).then(function (data) {
+  	res.status(200).json({
+  		status: 'success',
+  		data: data,
+  		message: 'GOT ONE USER FROM DATABASE: username' + username;
+  	})
+  }).catch(function (err) {
+  	return next(err);
+  });
+}
+
+function getSetting(req, res, next) {
+  var settings = req.params.id;
+  db.one('select * from Users where id = $1', settings).then(function (data) {
+  	res.status(200).json({
+  		status: 'success',
+  		data: data,
+  		message: 'GOT SETTINGS FROM DATABASE: ID' + id;
+  	});
+  }).catch(function (err) {
     return next(err);
   });
 }
 
-function getSingleUser(req, res, next) { // Get a single user from database
-  var username = req.params.username; // Access username
-  db.one('select * from User where username = $1', username).then(function (data) { // Get entry with this particular username...
-    res.status(200).json({ // and send a GET. 
-      status: 'success',
-      data: data,
-      message: 'GOT ONE USER FROM DATABASE: username' + username
-    });
-  }).catch(function (err) { // ERROR HANDLING
-    return next(err);
-  });
-}
-
-function getSetting(req, res, next) { // Get all settings
-  db.any('select * from Setting').then(function (data){ // Get all data from Settings database
-    res.status(200).json({// SEND 200 STATUS
-      status: 'success',
-      data: data,
-      message: 'RETREIVED ALL SETTINGS'
-    });
-  }).catch(function (err) { // ERROR HANDLING
-    return next(err);
-  });
-}
-
-function getBlackList(req, res, next) { // Get all blacklisted websites
-  db.any('select * from Url').then(function (data) { // Retrieves all the urls
-    res.status(200).json({// SEND 200 STATUS
+function getBlackList(req, res, next) {
+  db.one('select * from Url').then(function (data) {
+    res.status(200).json({
       status: 'success', 
       data: data,
-      message: 'RETRIEVED ALL FORBIDDEN WEBSITES'
-    });
-  }).catch(function (err) { // ERROR HANDLING
+      message: 'BLACKLISTED URLS'
+    })
+  }).catch(function (err) {
     return next(err);
   });
 }
 
-function getExtension(req, res, next) { // Get all extensions
-  db.any('select * from Extension').then(function (data) { // Retreive data
-    res.status(200).json({// SEND 200 STATUS
+function postBlackList(req, res, next) {
+  db.none('insert into Url(url, blacklist_type, blacklist_name)'
+    + 'values(${url} + ${blacklist_type} + ${blacklist_name})', req.body)
+  .then(function () {
+    res.status(201).json({
+      status: 'success', 
+      message: 'INSERTED NEW BLACKLIST'
+    })
+  }).catch(function (err) {
+    return next(err);
+  });
+}
+
+function getExtension(req, res, next) {
+  db.one('select * from Extension').then(function (data) {
+    res.status(200).json({
       status: 'success',
       data: data,
-      message: 'RETRIEVED ALL EXTENSIONS'
+      message: 'OBTAINED EXTENSIONS'
     });
   }).catch(function (err) {
     return next(err);
   });
 }
 
-function getSingleGoal(req, res, next) { // Get an individual goal
-  var id = req.params.id; // Get id of individual goal
-  db.one('select * from Goal where username = $1', id).then(function (data) { // Locate goal in SingleGoal...
+function getReflections(req, res, next) {
+  db.one('select * from Reflection').then(function (data) {
     res.status(200).json({
       status: 'success',
       data: data,
-      message: 'RETRIEVED GOAL: ID' + id
+      message: 'OBTAINED REFLECTIONS'
     });
-  }).catch(function (err) { // ERROR HANDLING
+  }).catch(function (err) {
+    return next(err);
+  });
+}
+
+function getReflectionId(req, res, next) {
+  var id = parseInt(req.params.id);
+  db.one('select * from Users where id = $1', id).then(function (data) {
+    res.status(200).json({
+      status: 'success',
+      data: data,
+      message: 'GOT ONE USER FROM DATABASE: id' + id;
+    })
+  }).catch(function (err) {
+    return next(err);
+  });
+}
+
+function postReflectionId(req, res, next) {
+  db.none('insert into Reflection(answer)'
+    + 'values(${answer})', req.body)
+  .then(function () {
+    res.status(201).json({
+      status: 'success', 
+      message: 'INSERTED NEW REFLECTION ID'
+    })
+  }).catch(function (err) {
     return next(err);
   })
 }
 
-function getReflections(req, res, next) {
-  db.any('select * from Reflection').then(function (data) {
-     res.status(200).json({ // SEND 200 CODE
-      status: 'success',
-      data: data,
-      message: 'RETRIEVED REFLECTIONS'
-    });
-  }).catch(function (err) { // ERROR HANDLING
-    return next(err);
-  });   
-}
-
-function getReflectionId(req, res, next) {
-  var id = req.params.id;
-  db.one('select * from Reflection where id = $1', id).then(function (data) {
+function getAllGoals(req, res, next) {
+  db.any('select * from Goal').then(function (data) {
     res.status(200).json({
       status: 'success',
       data: data,
-      message: 'RETRIEVED REFLECTION: ID' + id
+      message: 'RETREIVED ALL GOALS'
     });
-  }).catch(function (err) { // ERROR HANDLING
+  }).catch(function (err) {
     return next(err);
-  });
-}
-
-function getAllGoals(req, res, next) { // Get everything in goal schema
-  db.any('select * from Goal').then(function (data) { // GET ALL GOALS
-    res.status(200).json({ // SEND 200 STATUS
-      status: 'success',
-      data: data,
-      message: 'RETRIEVED GOALS'
-    });
-  }).catch(function (err) { // ERROR HANDLING
-    return next(err);
-  });
-}
-
-function getSubGoals(req, res, next) {  // Get subgoals
-  db.any('select * from Subgoal').then(function (data) { // GET ALL SUBGOAL DATA
-    res.status(200).json({ // SEND 200 CODE
-      status: 'success',
-      data: data,
-      message: 'RETRIEVED SUBGOALS'
-    });
-  }).catch(function (err) { // ERROR HANDLING
-    return next(err);
-  });
-}
-
-
-// POSTERS
-
-function postBlackList(req, res, next)  {
-  console.log("HELLO WORLD")
-}
-
-function postReflectionId(req, res, next) {
-  console.log("HELLO WORLD");
+  })
 }
 
 function postAllGoals(req, res, next) {
-  console.log("HELLO WORLD");
+  db.none('insert into Goal(goal, progress, goal_picture)'
+    + 'values(${goal}, ${progress}, ${goal_picture})' + req.body)
+  .then(function () {
+    res.status(201).json({
+      status: 'success'
+      message: 'POSTED GOALS'
+    });
+  }).catch(function (err) {
+    return next(err);
+  })
+}
+
+function getSingleGoal(req, res, next) {
+  var id = req.params.id
+  db.any('select * from Goal where id = $1', id).then(function (data) {
+    res.status(200).json({
+      status: 'success',
+      data: data,
+      message: 'RETREIVED ALL GOALS'
+    });
+  }).catch(function (err) {
+    return next(err);
+  })
 }
 
 function postSingleGoal(req, res, next) {
-  console.log("HELLO WORLD");
+  db.none('insert into Goal(goal, progress, goal_picture)'
+    + 'values($1, $2, $3)', [req.params.goal, req.params.progress, req.params.goal_picture])
+  .tnen(function () {
+    res.status(200).json({
+      status: 'success'
+      message: 'Inserted goals'
+    });
+  }).catch(function (err) {
+    return next(err);
+  })
 }
+
+
+function getSubGoals(req, res, next) {
+  db.any('select * from Subgoal').then(function(data) {
+    res.status(200).json({
+      status: 'success', 
+      data: data, 
+      message: 'RETREIVED ALL SUBGOALS'
+    });
+  }).catch(function (err) {
+    return next(err);
+  });
+}
+
 
 function postSubGoals(req, res, next) {
-  console.log("HELLO WORLD");
+  db.none('insert into Subgoal(subgoal, status)' 
+    + 'values(${subgoal} + ${status})', req.body)
+  .tnen(function () {
+    res.status(200).json({
+      status: 'success'
+      message: 'INSERTED SUBGOALS'
+    });
+  }).catch(function (err) {
+    return next(err);
+  })
 }
 
-module.exports = { // Module exports object.
-  getAllUsers: getAllUsers,
-  getSingleUser: getSingleUser,
+module.exports = {
+	getAllUsers: getAllUsers,
+	getSingleUser: getSingleUser,
   getSetting: getSetting,
   getBlackList: getBlackList,
+  postBlackList: postBlackList,
   getExtension: getExtension,
-  getSingleGoal: getSingleGoal,
   getReflections: getReflections,
   getReflectionId: getReflectionId,
-  getAllGoals: getAllGoals,
-  getSubGoals: getSubGoals,
-  postBlackList: postBlackList,
   postReflectionId: postReflectionId,
-  postAllGoals: postAllGoals,
+  getSingleGoal: getSingleGoal,
   postSingleGoal: postSingleGoal,
+  getSubGoals: getSubGoals,
   postSubGoals: postSubGoals
 }
