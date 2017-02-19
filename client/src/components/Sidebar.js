7c33eefd8adb8e4a0c5ce133a36b60f107ddaa1f
SherryH <enjoy.wind7rain@gmail.com>
2017-02-18 18:19:19 -0800

    Refactor Authentication Service

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Grid, Row, Col} from 'react-materialize'
-export const Sidebar =()=>(
+export const Sidebar =({auth})=>(
 
   <header>
       <nav className="top-nav full hide-on-large-only">
@@ -9,9 +9,8 @@ export const Sidebar =()=>(
       <ul id="slide-out" className="side-nav fixed theme-color">
         <li><div className="userView">
           <a href="#"><img className="circle" /></a>
-          <a href="#"><span className="name">John Doe</span></a>
-          <a href="#"><span className="email">jdandturk@gmail.com</span></a>
-          <a href="#" className="btn">Log Out</a>
+          <a href="#" className="btn" onClick={auth.login}>Log In</a>
+          <a href="#" className="btn" onClick={auth.logout}>Log Out</a>
         </div></li>
         <li><div className="divider"></div></li>
         <li><a href="#/dashboard" className="waves-effect"><i className="material-icons icon-white">dashboard</i>Dashboard</a></li>
--- client/src/index.js
+++ client/src/index.js
@@ -7,17 +7,27 @@ import Landingpage from './components/Landingpage.js';
 import Dashboard from './components/Dashboard.js';
 import Selfreflection from './components/Selfreflection.js';
 import Settings from './components/Settings.js';
+import AuthService from './utils/AuthService.js';
 
 const app = document.getElementById('app');
 
+const auth = new AuthService('V3oJOmHRIeW7nJnLZIa7ioWOo8godJVQ', 'focuspro.auth0.com');
+
+// validate authentication for private routes
+const requireAuth = (nextState, replace)=>{
+  if (!auth.loggedIn()){
+    replace({ pathname: '/' });
+  }
+};
+
 render(
   <Router history={hashHistory}>
-    <Route path="/" component = {App}>
+    <Route path="/" component = {App} auth={auth}>
     <IndexRoute component={Landingpage}></IndexRoute>
     <Route path="access_token=:token" component={Dashboard}/>
-    <Route path="dashboard" component={Dashboard}/>
-    <Route path="selfreflection" component={Selfreflection}/>
-    <Route path="settings" component={Settings}/>
+    <Route path="dashboard" component={Dashboard} onEnter={requireAuth}/>
+    <Route path="selfreflection" component={Selfreflection} onEnter={requireAuth}/>
+    <Route path="settings" component={Settings} onEnter={requireAuth}/>
     </Route>
   </Router>
   , app
