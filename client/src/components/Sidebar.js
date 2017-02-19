999f64090c23a4d39759b59fa54cee793d35706f
SherryH <enjoy.wind7rain@gmail.com>
2017-02-18 21:09:17 -0800

    Display user profile on log in

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react';
-import {Grid, Row, Col} from 'react-materialize'
-export const Sidebar =({auth})=>(
+import React, { Component} from 'react';
+// export const Sidebar =({auth})=>(
 
-  <header>
-      <nav className="top-nav full hide-on-large-only">
-          <div className="nav-wrapper"><a href="#" data-activates="slide-out" className="button-collapse top-nav full hide-on-large-only"><i className="material-icons">menu</i></a></div>
-      </nav>
-      <ul id="slide-out" className="side-nav fixed theme-color">
-        <li><div className="userView">
-          <a href="#"><img className="circle" /></a>
-          
-          <a href="#"><span className="name">John Doe</span></a>
-          <a href="#"><span className="email">jdandturk@gmail.com</span></a>
-          {!localStorage.id_token && <a href="#" className="btn" onClick={auth.login}>Log In</a>}
-          {localStorage.id_token &&<a href="#" className="btn" onClick={auth.logout}>Log Out</a>}
-        </div></li>
-        <li><div className="divider"></div></li>
-        <li><a href="#/dashboard" className="waves-effect"><i className="material-icons icon-white">dashboard</i>Dashboard</a></li>
-        <li><a href="#/selfreflection" className="waves-effect"><i className="material-icons icon-white">perm_identity</i>Self Reflections</a></li>
-        <li><a href="#/settings" className="waves-effect"><i className="material-icons icon-white">settings</i>Settings</a></li>
+export class Sidebar extends React.Component {
 
-      </ul>
-    </header>
-);
+
+  constructor(props){
+    super(props);
+    this.state = {
+      profile: this.props.auth.getProfile()
+    };
+    console.log('state profile', this.state.profile);
+    // listen to profile_updated events to update internal state
+    this.props.auth.event.on('profile_updated',(newProfile)=>{
+      console.log('event listened', newProfile);
+      this.setState({'profile': newProfile});
+    });
+  }
+
+  render(){
+    return (
+      <header>
+          <nav className="top-nav">
+            <div className="container">
+              <div className="nav-wrapper"><a className="page-title">FocusPro</a></div>
+            </div>
+          </nav>
+          <div className="container"><a href="#" data-activates="slide-out" className="button-collapse top-nav full hide-on-large-only"><i className="material-icons">menu</i></a></div>
+
+          <ul id="slide-out" className="side-nav fixed theme-color">
+            {localStorage.profile && <li><div className="userView">
+              <a href="#"><img className="circle" /></a>
+              <a href="#"><span className="name">{this.state.profile.nickname}</span></a>
+              <a href="#"><span className="email">{this.state.profile.email}</span></a>
+              <a href="#" className="btn" onClick={this.props.auth.logout}>Log Out</a>
+            </div></li>}
+
+            <li>
+                {!localStorage.id_token && <a href="#" className="btn" onClick={this.props.auth.login}>Log In</a>}
+            </li>
+            <li><div className="divider"></div></li>
+            <li><a href="#" className="waves-effect"><i className="material-icons icon-white">settings</i>Settings</a></li>
+            <li><a href="#" className="waves-effect"><i className="material-icons icon-white">perm_identity</i>Self Reflections</a></li>
+          </ul>
+      </header>
+    );
+  }
+}
+
+// );
+
--- client/src/utils/AuthService.js
+++ client/src/utils/AuthService.js
@@ -1,3 +1,4 @@
+import { EventEmitter } from 'events';
 import Auth0Lock from 'auth0-lock';
 import { browserHistory } from 'react-router';
 
@@ -15,13 +16,43 @@ export default class AuthService {
     this.lock.on('authenticated', this._doAuthentication.bind(this));
     // binds login functions to keep this context
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+
+    //set a profile property
+    this.profile = null;
+    this.event = new EventEmitter();
   }
 
   _doAuthentication(authResult) {
     // Saves the user token
     this.setToken(authResult.idToken);
+    this.lock.getUserInfo(authResult.accessToken,(error, profile)=>{
+      if (error) {
+        console.log('Error loading the Profile', error);
+      } else {
+        this.setProfile(profile);
+      }
+    });
     // navigate to the dashboard route
     browserHistory.replace({pathname: '/#/dashboard'});
+  }
+
+  setProfile(profile) {
+    console.log(profile);
+    console.log('stringify profile',JSON.stringify(profile));
+    localStorage.setItem('profile', JSON.stringify(profile));
+    this.profile = profile;
+    console.log('this.profile',this.profile);
+    // let event = new EventEmitter();
+    this.event.emit('profile_updated', profile);
+  }
+
+  getProfile(callback) {
+    const profile = localStorage.getItem('profile');
+    // callback && callback(JSON.parse(profile));
+    // console.log('get this.Profile',this.profile);
+    // return this.profile; //return null
+    return profile ? JSON.parse(profile) : {};
   }
 
   login() {
@@ -45,8 +76,9 @@ export default class AuthService {
   }
 
   logout() {
-    // Clear user token and profile data from local storage
+    console.log('logout',this.getProfile().name);
     localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
     browserHistory.replace('/');
   }
 }
