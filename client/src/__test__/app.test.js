73e7cee71d0e182b2daf6cb628b15e1ebeff62aa
SherryH <enjoy.wind7rain@gmail.com>
2017-02-20 20:47:38 -0800

    Add tests for App and Sidebar widget

--- client/src/__test__/app.test.js
+++ client/src/__test__/app.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import App from '../components/App.js';
 import Sidebar from '../components/Sidebar.js';
 import { shallow, mount } from 'enzyme';
+import {Col} from 'react-materialize';
 
 describe('App', ()=>{
   it('renders without crashing', ()=>{
     var route = {auth: 'dummy'};
     shallow(<App route={route}/>);
+  });
+  it('renders main section inside a materializeCSS grid column', ()=>{
+    var route = {auth: 'dummy'};
+    const wrapper = shallow(<App route={route}/>);
+    expect(wrapper.find(Col).contains(<div className="container" />)).toBeTruthy();
   });
 });
