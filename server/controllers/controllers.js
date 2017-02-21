0f59330a302f31da68ec01482d2941911d9e2e9f
igeorge <irgeorge@gmx.com>
2017-02-20 18:19:18 -0600

    Further code cleanup

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -3,98 +3,105 @@ var parser  = require('body-parser');
 var express = require('express');
 var app = express();
 
+
+
 // GETTERS
 
-function getAllUsers(req, res, next) { // Get all the users
-  db.User.findAll({}).then(function(data) { // Find all users
-    res.status(200).json({ // Send 200 status
+
+
+function getAllUsers(req, res, next) { // Get all the users.
+  db.User.findAll({}).then(function(data) { // Find all users.
+    res.status(200).json({ // Send 200 status upon success.
       status: 'success', 
       data: data, 
       message: 'RETREIVED ALL USERS'
     });
-  }).catch(function (err) { // Error handling
+  }).catch(function (err) { // Error handling for callback findAll.
     return next(err);
   });
 }
 
-function getSingleUser(req, res, next) { // Get specific user
-  var username = req.params.username; // Obtain specific username
-  db.User.find({where: {username: username}}).then(function (data) { // Find the user with the username...
-    res.status(200).json({ // Send 200 status
+function getSingleUser(req, res, next) { // Get specific user.
+  var username = req.params.username; // Obtain specific username.
+  db.User.find({where: {username: username}}).then(function (data) { // Find the user with the given username.
+    res.status(200).json({ // Send 200 status upon success.
       status: 'success',
       data: data,
       message: 'GOT USER FROM DATABASE: ' + username
     })
-  }).catch(function (err) { // Error handling
+  }).catch(function (err) { // Error handling for callback find.
     return next(err);
   });
 }
 
-function getSettings(req, res, next) { // Get settings for specific user
-  var username = req.params.username; // Obtain specific username
-  db.User.find({where: {username: username}}).then(function (user) { // Find user...
-    db.Setting.findAll({}).then(function (data) { // Grab settings data for user...
-      res.status(200).json({ // Send 200 status
+function getSettings(req, res, next) { // Get settings for specific user.
+  var username = req.params.username; // Obtain specific username.
+  db.User.find({where: {username: username}}).then(function (user) { // Find user with the given username.
+    db.Setting.findAll({}).then(function (data) { // Grab settings data for user.
+      res.status(200).json({ // Send 200 status upon success.
         status: 'success',
         data: data,
         message: 'GOT ALL SETTINGS FOR USER:  ' + username
       });
-    }).catch(function (err) { // Error handling
+    }).catch(function (err) { // Error handling for callback findAll.
       return next(err);
     });
   });
 }
 
+function getBlackList(req, res, next) {
+  var username = req.params.username; // Obtain username
+  db.User.find({where: {username: username}}).then(function (user) {
+    db.Url.findAll({}).then(function (data) { // Get all blacklist data.
+      res.status(200).json({ // Send 200 status upon success.
+        status: 'success', 
+        data: data,
+        message: 'BLACKLISTED URLS'
+      })
+    }).catch(function (err) { // Error handling for inner callback findAll.
+      return next(err);
+    });
+  }).catch(function (err) { // Error handling for outer callback find.
+    return next(err);
+  });
+}
+
+
 
 // SETTERS
 
-function postSettings(req, res, next) { // Post settings for specific user
-  var username = req.params.username; // Obtain specific username
-  db.User.find({where: {username: username}}).then(function (user) { // Grab settings data for user...
-    var setting = req.params.id; // Find settings parameters
+
+
+function postSettings(req, res, next) { // Post settings for specific user.
+  var username = req.params.username; // Obtain specific username.
+  db.User.find({where: {username: username}}).then(function (user) { // Grab settings data for given user.
+    var setting = req.params.id; // Find settings parameters.
     var picture = req.body.picture;
     var reflection_freq = req.body.reflection_freq;
     var reminder = req.body.reminder;
     var reminder_type = req.body.reminder_type;
     var reminder_freq = req.body.reminder_freq;
     var reminder_address = req.body.reminder_address;
-    // Create entry in Settings with above parameters
+    // Create entry in Settings with above parameters.
     db.Setting.create({picture: picture, reflection_freq: reflection_freq, reminder: reminder, reminder_type: reminder_type, reminder_freq: reminder_freq, reminder_address: reminder_address}).then(function (data) {
-      res.status(201).json({ // Send 201 status
+      res.status(201).json({ // Send 201 status upon success.
         status: 'success',
         data: data,
         message: 'GOT ALL SETTINGS FOR USER   ' + username
       });
-    }).catch(function (err) { // Error handling for create
+    }).catch(function (err) { // Error handling for inner callback create
       return next(err);
     });
-  }).catch(function (err) { // Error handling for find
+  }).catch(function (err) { // Error handling for outer callback find
     return next(err);
   })
 }
 
-function getBlackList(req, res, next) {
-  var username = req.params.username;
-  db.User.find({where: {username: username}}).then(function (user) {
-     db.Url.findAll({}).then(function (data) {
-    res.status(200).json({
-      status: 'success', 
-      data: data,
-      message: 'BLACKLISTED URLS'
-    })
-  }).catch(function (err) {
-    return next(err);
-  });
-  }).catch(function (err) {
-    return next(err);
-  });
- 
-}
 
-function postBlackList(req, res, next) {
+function postBlackList(req, res, next) { // Post blacklisted website for specific user.
    var username = req.params.username;
-  db.User.find({where: {username: username}}).then(function (user) {
-     db.Url.create({
+   db.User.find({where: {username: username}}).then(function (user) {
+   db.Url.create({
        url: req.body.url,
       blacklist_type: req.body.blacklist_type,
       blacklist_time: req.body.blacklist_time
