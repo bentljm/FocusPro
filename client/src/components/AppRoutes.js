99a33e1ff21ce3160057cbed31418a087e9a199b
igeorge <irgeorge@gmx.com>
2017-02-21 17:45:48 -0600

    Add routes to AppRoutes

--- client/src/components/AppRoutes.js
+++ client/src/components/AppRoutes.js
@@ -5,7 +5,18 @@ import LandingPage from './LandingPage.js';
 import Motivational from './Motivational.js';
 import SelfReflection from './Selfreflection.js';
 
-
+const clientRoutes = (
+  <Route path="/api/users" component={App}/>
+  <Route path="/api/users/:username" component={App}/>
+  <Route path="/api/users/:username/setting" component={App}/>
+  <Route path="/api/users/:username/setting/blacklist" component={App}/>
+  <Route path="/api/users/:username/extension_data" component={App}/>
+  <Route path="/api/users/:username/goals/:goal_id/reflections" component={App}/>
+  <Route path="/api/users/:username/goals/:goal_id/reflections/:reflection_id" component={App}/>
+  <Route path="/api/users/:username/goals" component={App}/>
+  <Route path="/api/users/:username/goals/:goal_id" component={App}/>
+  <Route path="api/users/:username/goals/:goal_id/subgoals" component={App}/>
+);
 
 export default class AppRoutes extends React.Component {
   render() {
