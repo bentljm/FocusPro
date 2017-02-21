ab5e3cbea8f9c276481a8e72efb7ce8ee46f6c2e
Josephine-Chen <jychen@g.hmc.edu>
2017-02-21 15:37:21 -0800

    Create new tab to web app on browser action, Console log new active tab

--- extension/background.js
+++ extension/background.js
@@ -1,17 +1,35 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+// // Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // Send a message to the active tab
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   });
+// });
+
+// // Listen for open new tab request to create a tab to focusProUrl
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "open_new_tab" ) {
+//       chrome.tabs.create({"url": request.url});
+//     }
+//   }
+// );
+
+
+//When clicked, create a new tab to FocusPro site
+chrome.browserAction.onClicked.addListener(function(){
+  var focusProURL = 'https://focuspro.herokuapp.com/';
+  chrome.tabs.create({"url": focusProURL});
 });
 
-// Listen for open new tab request to create a tab to focusProUrl
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
+//Call when active tab in window changes
+chrome.tabs.onActivated.addListener(function(tab) {
+  //tab contains object: id of the tab that was made active and id of its window
+  //Send info "to content to console log
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "new_tab_activated"});
+  });
+  // chrome.tabs.sendMessage(tab.id, {"message": "new_tab_activation"});
+});
--- extension/content.js
+++ extension/content.js
@@ -1,14 +1,15 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
+    if( request.message === "new_tab_activated" ) {
+      // window.alert('new tab activated');
       var firstHref = $("a[href^='http']").eq(0).attr("href");
 
       //This is site url
-      console.log(firstHref);
+      console.log('this site is', firstHref);
 
       //Send message to background.js to open a new tab to focusProURL
-      var focusProURL = 'https://focuspro.herokuapp.com/';
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": focusProURL});
+      // var focusProURL = 'https://focuspro.herokuapp.com/';
+      // chrome.runtime.sendMessage({"message": "open_new_tab", "url": focusProURL});
     }
   }
 );
