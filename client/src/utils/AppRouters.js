ee845dd3622272ac138019718acfb9bbefef6b50
igeorge <irgeorge@gmx.com>
2017-02-21 21:36:29 -0600

    Start AJAX routers

---
+++ client/src/utils/AppRouters.js
@@ -0,0 +1,221 @@
+function getAllUsers () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+function postUser (user) {
+	$.ajax({
+		type: 'POST',
+		url: '/api/users'
+		success: function (data) {
+          console.log("SUCCESS: POSTED ALL USERS: " + data)
+		}
+        error: function(err) {
+    	   throw err;
+        } 
+	})
+}
+
+function getSingleUser () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+function getSettings () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+function postSettings () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+function getBlackList () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+function postBlackList () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+function getExtension () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+function getReflections () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+function getReflectionId () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+
+
+function postReflectionId () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+
+
+
+function getAllGoals () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+
+
+
+function getSingleGoal () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+
+
+function postSingleGoal () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+
+
+
+function getSubGoals () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
+
+
+
+function postSubGoals () {
+  $.ajax({
+    type: 'GET',
+    url: '/api/users',
+    success: function(data) {
+      console.log("SUCCESS: OBTAINED ALL USERS: " + data)
+    };
+    error: function(err) {
+    	throw err;
+    }
+  })
+}
