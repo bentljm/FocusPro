1b4c440ec676110cd332a6c13f606a5fa2560cfa
Josephine-Chen <jychen@g.hmc.edu>
2017-02-22 19:30:33 -0800

    Add goals and get goals functionality works in dashboard

--- server/routers.js
+++ server/routers.js
@@ -9,30 +9,30 @@ module.exports = function(app, express) {
 
   app.get('/api/users/:auth0_id', controller.getSingleUser); // Get individual name
 
-  app.get('/api/users/:username/setting', controller.getSettings); // Get individual settings
+  app.get('/api/users/:auth0_id/setting', controller.getSettings); // Get individual settings
 
-  app.post('/api/users/:username/setting', controller.postSettings); // Post individual settings
+  app.post('/api/users/:auth0_id/setting', controller.postSettings); // Post individual settings
 
-  app.get('/api/users/:username/setting/blacklist', controller.getBlackList); // Get individual blacklist
+  app.get('/api/users/:auth0_id/setting/blacklist', controller.getBlackList); // Get individual blacklist
 
-  app.post('/api/users/:username/setting/blacklist', controller.postBlackList); // Post or update individual blacklist
+  app.post('/api/users/:auth0_id/setting/blacklist', controller.postBlackList); // Post or update individual blacklist
 
-  app.get('/api/users/:username/extension_data', controller.getExtension); // Get individual extention data
+  app.get('/api/users/:auth0_id/extension_data', controller.getExtension); // Get individual extention data
 
-  app.get('/api/users/:username/goals/:goal_id/reflections', controller.getReflections); // Get individual reflection data
+  app.get('/api/users/:auth0_id/goals/:goal_id/reflections', controller.getReflections); // Get individual reflection data
 
-  app.get('/api/users/:username/goals/:goal_id/reflections/:reflection_id', controller.getReflectionId); // Get individual reflection id
+  app.get('/api/users/:auth0_id/goals/:goal_id/reflections/:reflection_id', controller.getReflectionId); // Get individual reflection id
 
-  app.post('/api/users/:username/goals/:goal_id/reflections', controller.postReflectionId); // Post individual reflection id
+  app.post('/api/users/:auth0_id/goals/:goal_id/reflections', controller.postReflectionId); // Post individual reflection id
 
-  app.get('/api/users/:username/goals', controller.getAllGoals); //Get all goals
+  app.get('/api/users/:auth0_id/goals', controller.getAllGoals); //Get all goals
 
-  app.get('/api/users/:username/goals/:goal_id', controller.getSingleGoal); //Get single goal
+  app.get('/api/users/:auth0_id/goals/:goal_id', controller.getSingleGoal); //Get single goal
 
-  app.post('/api/users/:username/goals', controller.postSingleGoal); //Post single goal
+  app.post('/api/users/:auth0_id/goals', controller.postSingleGoal); //Post single goal
 
-  app.get('/api/users/:username/goals/:goal_id/subgoals', controller.getSubGoals); //Get all subgoals of a single goal
+  app.get('/api/users/:auth0_id/goals/:goal_id/subgoals', controller.getSubGoals); //Get all subgoals of a single goal
 
-  app.post('/api/users/:username/goals/:goal_id/subgoals', controller.postSubGoal); //Post all subgoals of a single goal
+  app.post('/api/users/:auth0_id/goals/:goal_id/subgoals', controller.postSubGoal); //Post all subgoals of a single goal
 
 };
