0af96fd104579fab95ccd1d16b4c4b5bfb3022d2
SherryH <enjoy.wind7rain@gmail.com>
2017-02-24 12:35:47 -0800

    Fix syntax error on reflection tests

--- server/test/reflections.test.js
+++ server/test/reflections.test.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var app = express();
 app.use(bodyparser.json());
-router(app,'');
+router(app, '');
 const pg = require('pg');
 const db = require('../databases/Schema.js');
 
@@ -28,31 +28,31 @@ before((done)=>{
 
   //set up dummy user
   var user1 = {username: 'dummy4', auth0_id: 'auth_id4', daily_goal: 'sleep earlier than yesterday'};
-  db.User.create(user1).then(function(user){
+  db.User.create(user1).then(function(user) {
     global.UserId = user.id;
-        done();
+    done();
   });
 });
 
 //close DB connection
 after((done)=>{
   //delete all users in db
-  db.User.destroy({where:{}}).then((num) => {
+  db.User.destroy({where: {}}).then((num) => {
     done();
   });
   global.client.end();
   done();
 });
 
-describe('GET and POST requests to /api/users/:auth0_id/reflections',()=>{
+describe('GET and POST requests to /api/users/:auth0_id/reflections', ()=>{
   beforeEach((done)=>{
     //post a reflection
-    db.Reflection.create({auth0_id:'auth_id4', answer:answer2, question:question2});
-    db.Reflection.create({auth0_id:'auth_id4', answer:answer3, question:question3}).then(()=>{done();});
+    db.Reflection.create({auth0_id: 'auth_id4', answer: answer2, question: question2});
+    db.Reflection.create({auth0_id: 'auth_id4', answer: answer3, question: question3}).then(()=>{ done(); });
   });
   describe('POST a new reflection', ()=>{
-    it('/api/users/:auth0_id/reflections creates user reflections',(done)=>{
-      var reflection = {auth0_id:'auth_id4', answer:answer1, question:question1};
+    it('/api/users/:auth0_id/reflections creates user reflections', (done)=>{
+      var reflection = {auth0_id: 'auth_id4', answer: answer1, question: question1};
       request(app)
       .post('/api/users/auth_id4/reflections')
       .send(reflection)
@@ -60,21 +60,21 @@ describe('GET and POST requests to /api/users/:auth0_id/reflections',()=>{
         expect(res.body.data.auth0_id).to.equal('auth_id4');
         expect(res.body.data.question).to.equal(question1);
         expect(res.body.data.answer).to.equal(answer1);
-        db.Reflection.destroy({where:{}}).then(()=>{
-         done();
+        db.Reflection.destroy({where: {}}).then(()=>{
+          done();
         });
       });
     });
   });
 
   describe('Get reflections', ()=>{
-    it('/api/users/:auth0_id/reflections fetches user reflections',()=>{
+    it('/api/users/:auth0_id/reflections fetches user reflections', ()=>{
       request(app)
       .get('/api/users/auth_id4/reflections')
       .end((err, res)=>{
         expect(res.body.data.length).to.equal(2);
-        expect(res.body.data.some((reflection) =>reflection.question===question2)).to.equal.true;
-        expect(res.body.data.some((reflection) =>reflection.answer===answer3)).to.equal.true;
+        expect(res.body.data.some((reflection) =>reflection.question === question2)).to.equal.true;
+        expect(res.body.data.some((reflection) =>reflection.answer === answer3)).to.equal.true;
       });
     });
   });
