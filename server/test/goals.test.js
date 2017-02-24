6c312df39c8b81bbae967684dd484d66cf664b44
SherryH <enjoy.wind7rain@gmail.com>
2017-02-24 12:24:32 -0800

    Refactor the goal tests

--- server/test/goals.test.js
+++ server/test/goals.test.js
@@ -26,13 +26,24 @@ after(() =>{
   global.client.end();
 });
 
-describe('GET and POST requests to /api/users/:auth0_id/goals', () => {
+describe('GET and POST and DELETE requests to /api/users/:auth0_id/goals', () => {
     //load dummy data
   beforeEach((done) =>{
     var user1 = {username: 'dummy3', auth0_id: 'auth_id3', daily_goal: 'wakeup earlier than yesterday'};
     db.User.create(user1).then(function(user) {
       global.UserId = user.id;
       global.authId = user.auth0_id;
+    })
+    .then(()=>{
+      const goalA = {goal: 'Mow Lawn', progress: 5, goal_picture: 'Picture', UserId: UserId};
+      return db.Goal.create(goalA)
+      .then((goal)=>{
+        global.goalId = goal.id;
+        global.subgoalAPI = '/api/goals/' + goal.id + '/subgoals';
+        global.subgoalPOSTReq = {subgoal: 'front yard', status: false};
+      });
+    })
+    .then(()=>{
       done();
     });
   });
@@ -52,7 +63,7 @@ describe('GET and POST requests to /api/users/:auth0_id/goals', () => {
     // done();
   });
 
-  describe('POST a new goal', () =>{
+  describe('POST a new goal or subgoal', () =>{
     it('/api/users/:auth0_id/goals creates a goal', (done) =>{
       const goalA = {goal: 'Mow Lawn', progress: 10, goal_picture: 'Picture'};
       request(app)
@@ -71,90 +82,84 @@ describe('GET and POST requests to /api/users/:auth0_id/goals', () => {
     });
 
     it('/api/goals/:goal_id/subgoals creates a subgoal', (done) =>{
-      const goalA = {goal: 'Mow Lawn', progress: 5, goal_picture: 'Picture', UserId: UserId};
-      db.Goal.create(goalA)
-      .then((goal)=>{
-        var subgoalAPI = '/api/goals/' + goal.id + '/subgoals';
-        var subgoalReq = {subgoal:'front yard', status: false};
-        // console.log('subgoalAPI',subgoalAPI);
-        // console.log('subgoalReq',subgoalReq);
-        request(app)
-        .post(subgoalAPI)
-        .send(subgoalReq)
-        .end((err, res)=>{
-          if (err) {
-            console.error('POST /subgoal \n', err);
-          }
-          // console.log(res.body);
-          expect(res.statusCode).to.equal(201);
-          expect(res.body.data.GoalId).to.equal(goal.id);
-          expect(res.body.data.subgoal).to.equal(subgoalReq.subgoal);
-          expect(res.body.data.status).to.equal(subgoalReq.status);
-        });
+      request(app)
+      .post(subgoalAPI)
+      .send(subgoalPOSTReq)
+      .end((err, res)=>{
+        if (err) {
+          console.error('POST /subgoal \n', err);
+        }
+        expect(res.statusCode).to.equal(201);
+        expect(res.body.data.GoalId).to.equal(goalId);
+        expect(res.body.data.subgoal).to.equal(subgoalPOSTReq.subgoal);
+        expect(res.body.data.status).to.equal(subgoalPOSTReq.status);
+        done();
+      });
+    }); //end it
+  });
+
+  describe('GET all goals or subgoals', () =>{
+    it('/api/users/:auth0_id/goals fetches all goals given user has goals', (done) =>{
+      var reqAPI = `/api/users/${authId}/goals`;
+      request(app)
+      .get(reqAPI)
+      .end((err, res) =>{
+        if (err) {
+          console.error('GET /api/users \n', err);
+        }
+        // console.log('get res', res.body.data);
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.data.some((goal) =>goal.goal === 'Mow Lawn')).to.be.true;
         done();
       });
     });
-  });
 
-  describe('GET all goals', () =>{
-    it('/api/users/:auth0_id/goals fetches all goals given user has goals', (done) =>{
-      var goal = {goal: 'Mow Lawn', progress: 10, goal_picture: 'Picture', UserId: UserId};
-      // console.log('goal obj',goal);
-      var reqAPI = `/api/users/${authId}/goals`;
-      // console.log('auth API', reqAPI);
-      db.Goal.create(goal).then(function(goal) {
-        // console.log('goal created',goal.goal); //UserID not created
+    it('/api/users/:auth0_id/goals fetches no goal given user has no goals', (done) =>{
+      db.Goal.destroy({where:{}}).then(()=>{
         request(app)
-        .get(reqAPI)
+        .get('/api/users/auth_id3/goals')
         .end((err, res) =>{
           if (err) {
             console.error('GET /api/users \n', err);
           }
-          // console.log('get res', res.body.data);
           expect(res.statusCode).to.equal(200);
-          expect(res.body.data.some((goal) =>goal.goal === 'Mow Lawn')).to.be.true;
+          expect(res.body.data.length).to.equal(0);
           done();
         });
       });
     });
-  });
 
-  it('/api/users/:auth0_id/goals fetches no goal given user has no goals', (done) =>{
-    request(app)
-    .get('/api/users/auth_id3/goals')
-    .end((err, res) =>{
-      if (err) {
-        console.error('GET /api/users \n', err);
-      }
-      expect(res.statusCode).to.equal(200);
-      expect(res.body.data.length).to.equal(0);
-      done();
-    });
-  });
-
-  it('/api/goals/:goal_id/subgoals', (done)=>{
-    var goal = {goal: 'Mow Lawn', progress: 30, goal_picture: 'Picture', UserId: UserId};
-    var goalId, endpoint;
-    db.Goal.create(goal).then(function(goal) {
-      // console.log('goal in subgoal', goal);
-      goalId = goal.id;
-      endpoint = `/api/goals/${goalId}/subgoals`;
-      // console.log('endpt', endpoint);
-    })
-    .then(()=>{
-      db.Subgoal.create({subgoal:'front yard', status: false, GoalId: goalId})
+    it('/api/goals/:goal_id/subgoals fetches all subgoals', (done)=>{
+      db.Subgoal.create({subgoal: 'front yard', status: false, GoalId: goalId})
       .then((subgoal)=>{
         request(app)
-        .get(endpoint)
+        .get(subgoalAPI)
         .end((err, res)=>{
           // console.log('subgoal', res.body,subgoal);
+          expect(res.body.data.length).to.equal(1);
+          expect(res.body.data[0].subgoal).to.equal('front yard');
+          expect(res.body.data[0].status).to.equal.false;
           done();
         });
-
       });
     });
 
+    it('/api/goals/:goal_id/subgoals fetches no subgoal given no subgoal present', (done)=>{
+      db.Subgoal.destroy({where: {}})
+      .then(()=>{
+        request(app)
+        .get(subgoalAPI)
+        .end((err, res)=>{
+          expect(res.body.data.length).to.equal(0);
+          done();
+        });
+      });
+    });
+  });//end describe GET
+
+  describe('DELETE a goal or a subgoal', ()=>{
+    //when a goal is removed, all subgoals get removed too
   });
 
-});
+});//end describe GET POST DELETE
 
