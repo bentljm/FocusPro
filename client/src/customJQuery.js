e4edf546626f370cb60fbca4ee80b5db8e9d362d
Jeff Bentler <bentljm@gmail.com>
2017-02-24 19:16:51 -0800

    Center name and email in sidebar

--- client/src/customJQuery.js
+++ client/src/customJQuery.js
@@ -9,9 +9,8 @@ $( document ).ready(function() {
       //edge: 'left', // Choose the horizontal origin
       // closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
+    });
 
-    }
-  );
   //For textarea
   $('#textarea1').val('');
   $('#textarea1').trigger('autoresize');
