e0e4fdf530747bd6e04bc9221cb9963707e62b79
Jeff Bentler <bentljm@gmail.com>
2017-02-27 21:18:48 -0800

    Remove username and email from Schema

--- server/databases/Schema.js
+++ server/databases/Schema.js
@@ -12,9 +12,7 @@ console.log('Successfully connected to PostgreSQL database');
 
 //User schema
 var User = db.define('User', {
-  username: Sequelize.STRING,
   auth0_id: {type: Sequelize.STRING, unique: true},
-  email: Sequelize.STRING,
   daily_goal: Sequelize.STRING
 });
 
