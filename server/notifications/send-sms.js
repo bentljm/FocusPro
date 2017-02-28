f9a99504608c92398c91f61eefdccf9fba0fe760
Josephine-Chen <jychen@g.hmc.edu>
2017-02-27 22:13:10 -0800

    Comment out nodemailer due to node issue

--- server/notifications/send-sms.js
+++ server/notifications/send-sms.js
@@ -1,18 +1,18 @@
-// Twilio Credentials 
+// Twilio Credentials
 var accountSid = 'ACfa9fe10bf0c085da49bcc44f84902449'; // Your Account SID from www.twilio.com/console
-var authToken = '0d8cca688dd9dc46a9b8849ff27f7a50';   
- 
-//require the Twilio module and create a REST client 
-var client = require('twilio')(accountSid, authToken); 
- 
+var authToken = process.env.TWILIO_AUTH || require('../config/config.js').TWILIO_AUTH;
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
+
 var sendText = function(recipientNumber) {
 	var number = "'+1" + recipientNumber + "'";
-	client.messages.create({ 
-	    to: number, 
-	    from: "+14252303081", 
-	    body: "Welcome to FocusPro!", 
-	}, function(err, message) { 
-	    console.log(err, message); 
+	client.messages.create({
+	    to: number,
+	    from: "+14252303081",
+	    body: "Welcome to FocusPro!",
+	}, function(err, message) {
+	    console.log(err, message);
 	});
 };
 
