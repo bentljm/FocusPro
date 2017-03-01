f47e2054e3e6f663dab60e7e51e77c4be67fa8b1
igeorge <irgeorge@gmx.com>
2017-03-01 17:34:54 -0600

    Fix component

--- client/src/components/Selfreflection.js
+++ client/src/components/Selfreflection.js
@@ -6,14 +6,14 @@ export default class Selfreflection extends React.Component {
     console.log(props);
     super(props);
     this.state = {
-      profile: this.props.auth.getProfile(),
+      profile: this.props.auth.getProfile(), // it gets imported here
       reflections: [],
       answer: '',
+      question: '',
     };
-    this.getAllReflections = this.getReflections.bind(this);
-    this.getAllGoals = this.getAllGoals.bind(this);
-    this.postReflections = this.postReflections.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.getReflections = this.getReflections.bind(this);
+    //this.postReflections = this.postReflections.bind(this);
+    //this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
@@ -26,69 +26,63 @@ export default class Selfreflection extends React.Component {
     $('.collapsible').collapsible();
   }
 
-  getAllGoals() {
-    var that = this;
-    $.ajax({
-      type: 'GET',
-      url: '/api/users/' + this.props.user_id + '/goals',
-      success: function(data) {
-        console.log("OBTAINED ALL GOALS")
-      },
-      error: function(err) {console.error("ERROR: COULD NOT GET GOALS")}
-    })
-  }
-
   getReflections() { //  Get question here
     var that = this;
     $.ajax({
       type: 'GET', // GET REQUEST
-      url: '/api/users/'+ this.props.user_id + '/reflections',
+      url: '/api/users/'+ this.state.profile.user_id + '/reflections',
       success: function(data) {
-        console.log("SUCCESS: OBTAINED REFLECTIONS: ", data.data);
-        that.setState({question: data.data.question});
+        console.log(data.data)
+        console.log("USER ID: ", that.state.profile.user_id)
+        console.log("SUCCESS: OBTAINED REFLECTIONS: ", data);
+        that.setState({reflections: data.data});
       },
       error: function(err) {console.log("ERROR: COULD NOT GET REFLECTIONS", err)}
     });
   }
+
+  /*
   
   postReflections() {
     $.ajax({
       type: 'POST',
-      url: '/api/users/'+ this.props.user_id + '/reflections',
+      url: '/api/users/'+ this.state.profile.user_id + '/reflections',
       contentType: 'application/json',
-      data: JSON.stringify({answer: this.state.answer, question: question, reflId: this.props.id}),
+      data: JSON.stringify({answer: this.state.answer, question: this.state.question, auth0_id: this.state.profile.user_id}),
       success: function(data) {console.log("SUCCESS: POSTED REFLECTIONS: ", data.data);},
       error: function(err) {console.log("ERROR: COULD NOT POST REFLECTION", err);}
     });
   }
+  
 
-  handleChange(event) {
+  handleAnswerChange(event) {
     this.setState({answer: event.target.value})
   }
+
+  handleQuestionChange(event) {
+    this.setState({question: event.target.value})
+  }
+  */
+  // format this to Date
 
   render() {
     return (
       <div>
         <h1> Self-Reflection </h1>
+        {this.state.reflections.map((reflection) => (
         <ul className="collapsible" data-collapsible="expandable">
           <li>
-            {this.state.reflections.map((reflection, index) => (
               <div className="collapsible-header">
-                <span className="questionDate">02/17/17</span><span className="questionDate">|</span>
-                <span>Did you spend adequate time towards your goal today?</span>
+                <span className="questionDate">{reflection.createdAt}</span><span className="questionDate"></span>
+                <span>{reflection.question}</span>
               </div>
               <div className="collapsible-body">
-                <span>Answer here.</span>
-
-                <Reflection key = {index} reflection = {reflection} answer = {reflection.answer}
-                question = {reflection.question} id = {reflection.id} user_id={this.props.user_id} />
-               
-              <input s={8} label="New Answer" onChange={this.handleChange}/>
-              <button className="submitAnswer" onClick={this.postReflections}>Self Reflection</button>
-            </div>
-             ))}
+                <span>{reflection.answer}</span>  
+              </div>        
           </li>
         </ul>
+        ))}
       </div>
     );
   }
+}
