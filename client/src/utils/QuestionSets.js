563bfc3e15aa16bf8c8e77afb1d5741d9325d58d
SherryH <enjoy.wind7rain@gmail.com>
2017-03-03 20:34:14 -0800

    Refactor questions to separate file

---
+++ client/src/utils/QuestionSets.js
@@ -0,0 +1,21 @@
+export const questionSet1 = {
+  feedback: 'Well done! <br /> Looks like you are on track to achieve your ultimate dream goal :D <br /> Keep it up as consistency is the key!',
+  questions:['What is already working in your system that can help enhacing self-discipline further?'],
+};
+export const questionSet2 = {
+  feedback: 'Very good! You have shown good discipline today :D <br /> It seems that you could have done better at tracking the time spent on the distraction site.',
+  questions:['How would performance change if you are more aware of how your time is spent?',
+  'What system can you implement to help you achieve that?'],
+};
+export const questionSet3 = {
+  feedback: 'Not bad, you are aware of how much time you lost on the distraction site so you knew that you still went over your allocated time. <br /> Please think about:',
+  questions:['What is already working in your system that you can build on?',
+  'What might "help" look like?'],
+};
+export const questionSet4 = {
+  feedback: 'Oops, not knowing how much time you spent on distraction sites... it seems like you are not on track to achieving you dream goal! Setting smaller yet achievable goals/rules is better than not achieving anything. <br /> Please think about:',
+  questions:['In the beginning, how did you want it to be?',
+  'What is the best/worst thing that could happen if you continue this way?',
+  'What might "help" look like?',
+  'How can you make yourself accountable?'],
+};
