af4653cc64cc5b2d69ff45e55104f68a4963e4f4
SherryH <enjoy.wind7rain@gmail.com>
2017-03-05 15:37:47 -0800

    Tidy up code

--- client/src/components/Selfreflection.js
+++ client/src/components/Selfreflection.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Row, Input, Button } from 'react-materialize';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-import moment from 'moment'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import moment from 'moment';
 import { getReflectionsAjax } from '../utils/SettingsUtil';
 
 export default class Selfreflection extends React.Component {
@@ -12,26 +11,14 @@ export default class Selfreflection extends React.Component {
       reflections: [],
       answer: '',
       question: '',
-      myTableData: [
-        {name: 'Rylan', email: 'Angelita_Weimann42@gmail.com'},
-        {name: 'Amelia', email: 'Dexter.Trantow57@hotmail.com'},
-        {name: 'Estevan', email: 'Aimee7@hotmail.com'},
-        {name: 'Florence', email: 'Jarrod.Bernier13@yahoo.com'},
-        {name: 'Tressa', email: 'Yadira1@hotmail.com'},
-      ],
-      products: [{
-            id: 1,
-            name: "Product1",
-            price: 120
-        }, {
-            id: 2,
-            name: "Product2",
-            price: 80
-        }],
     };
     this.getReflections = this.getReflections.bind(this);
-    // this.postReflections = this.postReflections.bind(this);
     this.handleCAnswerhange = this.handleAnswerChange.bind(this);
+
+    this.options = {
+      defaultSortName: 'date',  // default sort column
+      defaultSortOrder: 'asc',  // default sort order
+    };
   }
 
   componentDidMount() {
@@ -39,11 +26,6 @@ export default class Selfreflection extends React.Component {
     this.callCustomJQuery();
   }
 
-/*
-        that.setState({
-          reflections: data.data
-        });
-*/
   getReflections() {
     const that = this;
     getReflectionsAjax(this.state.profile.user_id, (data) => {
@@ -52,36 +34,25 @@ export default class Selfreflection extends React.Component {
       (data.data).forEach((refl, index) => {
         reflectionData[index].date = moment(refl.updatedAt).calendar();
       });
-      console.log('reflection.date', reflectionData);
-      console.log('reflection.date', data.data);
       that.setState({
         reflections: reflectionData,
       });
     });
   }
 
-  callCustomJQuery() {
-    $('.collapsible').collapsible();
+  getCaret(direction) {
+    if (direction === 'asc') {
+      return ( <i className="fa fa-caret-up" aria-hidden="true" /> );
+    }
+    if (direction === 'desc') {
+      return (
+        <i className="fa fa-caret-down" aria-hidden="true" />
+      );
+    }
+    return (
+      <i className="fa fa-sort" aria-hidden="true" />
+    );
   }
-
-  // postReflections() {
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: `/api/users/${this.state.profile.user_id}/reflections`,
-  //     contentType: 'application/json',
-  //     data: JSON.stringify({
-  //       answer: this.state.answer,
-  //       question: this.state.question,
-  //       auth0_id: this.state.profile.user_id
-  //     }),
-  //     success: (data) => {
-  //       console.log('SUCCESS: POSTED REFLECTIONS: ', data);
-  //     },
-  //     error: (err) => {
-  //       console.log('ERROR: COULD NOT POST REFLECTION', err);
-  //     }
-  //   });
-  // }
 
   handleAnswerChange(event) {
     this.setState({
@@ -89,20 +60,8 @@ export default class Selfreflection extends React.Component {
     });
   }
 
-  getCaret(direction) {
-    if (direction === 'asc') {
-      return (
-        <i className="fa fa-caret-up" aria-hidden="true"></i>
-      );
-    }
-    if (direction === 'desc') {
-      return (
-        <i className="fa fa-caret-down" aria-hidden="true"></i>
-      );
-    }
-    return (
-      <i className="fa fa-sort" aria-hidden="true"></i>
-    );
+  callCustomJQuery() {
+    $('.collapsible').collapsible();
   }
 
   render() {
@@ -110,14 +69,12 @@ export default class Selfreflection extends React.Component {
       <div>
         <h1> Self-Reflection </h1>
 
-        <BootstrapTable data={this.state.reflections} striped hover pagination>
-            <TableHeaderColumn hidden isKey dataField="id">ID</TableHeaderColumn>
-            <TableHeaderColumn dataSort dataField="question" tdStyle={{ whiteSpace: "normal" }} caretRender={ this.getCaret }>Question </TableHeaderColumn>
-            <TableHeaderColumn dataField="answer" tdStyle={ { whiteSpace: "normal" } }>Answer </TableHeaderColumn>
-            <TableHeaderColumn dataSort dataField="date" tdStyle={ { whiteSpace: "normal" } } caretRender={ this.getCaret } width="100">Date </TableHeaderColumn>
+        <BootstrapTable data={this.state.reflections} options={this.options} striped hover pagination>
+          <TableHeaderColumn hidden isKey dataField="id">ID</TableHeaderColumn>
+          <TableHeaderColumn dataSort dataField="question" tdStyle={{ whiteSpace: 'normal' }} caretRender={this.getCaret}>Question </TableHeaderColumn>
+          <TableHeaderColumn dataField="answer" tdStyle={{ whiteSpace: 'normal' }}>Answer </TableHeaderColumn>
+          <TableHeaderColumn dataSort dataField="date" tdStyle={{ whiteSpace: 'normal' }} caretRender={this.getCaret} width="100">Date </TableHeaderColumn>
         </BootstrapTable>
-
-
       </div>
     );
   }
