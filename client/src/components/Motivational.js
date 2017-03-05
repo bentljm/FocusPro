ce8eeae1666ca2a1f78926e1f6255ca363807d4d
igeorge <irgeorge@gmx.com>
2017-03-04 18:42:54 -0600

    Rebasing

--- client/src/components/Motivational.js
+++ client/src/components/Motivational.js
@@ -19,7 +19,7 @@ export default class Motivational extends React.Component {
           <p>What would your ideal 60th birthday look like?</p>
           <p>What would be the biggest regret you could have at the end of your life?</p>
           <p>18 months from now, what achievement would have the biggest positive impact on your life with respect to your theme?</p>
-          <p>Have you ever had the feeling that a discipline or field was tailor-made just for you?</p>
+          <p>Have you ever had the feeling that a discipline or field was tailor-made for you personally?</p>
           <p>What completed consumes your attention once you start doing it?</p>
           <p>What makes your heart race with excitement?</p>
           <p>What do you believe is worthwhile doing, that other people might not agree on?</p>
