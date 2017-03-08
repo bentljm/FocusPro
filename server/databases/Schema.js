53510993295a3dda42a5ffcbc8daebd2d9c0189a
igeorge <irgeorge@gmx.com>
2017-03-08 12:08:00 -0600

    Fix merge

--- server/databases/Schema.js
+++ server/databases/Schema.js
@@ -73,7 +73,6 @@ var Friendship = db.define('Friendship', {
 });
 
 //Instantiate relations
-User.hasMany(Goal, {foreignKey: 'auth0_id'});
 User.hasOne(Setting);
 User.hasMany(Extension);
 User.hasMany(Url, {foreignKey: 'auth0_id', sourceKey: 'auth0_id', constraints: false});
